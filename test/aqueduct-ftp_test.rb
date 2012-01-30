a76796e1c577c49601dfedc84d69dfa3daa21eb3
Remo Mueller <remosm@gmail.com>
2012-01-30 14:08:50 -0500

    Generated dummy app schema for TravisCI tests

--- test/aqueduct-ftp_test.rb
+++ test/aqueduct-ftp_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class AqueductFtpTest < ActiveSupport::TestCase
   test "truth" do
-    assert_kind_of Module, AqueductFtp
+    assert_kind_of Module, Aqueduct::Ftp
   end
 end
